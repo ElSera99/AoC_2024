f645b15ce60489ab082619af790a80e82c312afb
Serafin Tierrafria Baez <86864464+ElSera99@users.noreply.github.com>
2024-12-11 04:47:38 +0000

    Modify variables and names

--- day_1/day_1_1.py
+++ day_1/day_1_1.py
@@ -1,10 +1,12 @@
 """
-Proposed solution for advent of code 2024 day 1
+Proposed solution for advent of code 2024 day 1: Part 2
 """
+
+import pathlib
 from typing import List
 
-# Read data
-def obtain_id_lists(file_path: str, sep: str) -> tuple[list[int], list[int]]:
+
+def obtain_id_lists(file_name: str, sep: str) -> tuple[List[int], List[int]]:
     """
     Reads a list of integers from a file
 
@@ -12,40 +14,47 @@ def obtain_id_lists(file_path: str, sep: str) -> tuple[list[int], list[int]]:
         file_path: Path to the file
 
     Returns:
-        list: List of integers
+        tuple: Left and right lists of ids
     """
-    int_input = []
-    with open(file_path, 'r', encoding="UTF-8") as file:
+    int_input: list = []
+    absolute_path: str = pathlib.Path(__file__).parent.resolve()
+    full_file_path: str = absolute_path.joinpath(file_name)
+    with open(full_file_path, "r", encoding="UTF-8") as file:
         lines = file.readlines()
         for line in lines:
             elements = line.split(sep)
             elements = [int(element.strip().replace("\n", "")) for element in elements]
             int_input.append(elements)
 
-    list_1 = [element[0] for element in int_input]
-    list_2 = [element[1] for element in int_input]
+    left_list = [element[0] for element in int_input]
+    right_list = [element[1] for element in int_input]
 
-    return list_1, list_2
+    return left_list, right_list
 
-def obtain_total_distance(list_1: List[int], list_2: List[int]) -> List[int]:
+
+def obtain_total_distance(left_list: List[int], right_list: List[int]) -> int:
     """
     Obtains the total distance between the two lists
 
     Args:
-        list_1: List of integers
-        list_2: List of integers
+        list_1: List of ids
+        list_2: List of ids
 
     Returns:
-        list: List of integers
+        int: calculated total distance
     """
-    sorted_list_1 = sorted(list_1)
-    sorted_list_2 = sorted(list_2)
 
-    total_distance = [abs(sorted_list_1[i] - sorted_list_2[i]) for i in range(len(list_1))]
+    sorted_list_1 = sorted(left_list)
+    sorted_list_2 = sorted(right_list)
+
+    total_distance = [
+        abs(sorted_list_1[i] - sorted_list_2[i]) for i in range(len(left_list))
+    ]
 
     return sum(total_distance)
 
-# Sort data
-l1, l2 = obtain_id_lists(r"C:\Users\uif72896\Desktop\AoC_2024\day_1\input.txt", "   ")
-result = obtain_total_distance(l1, l2)
-print(result)
+
+if __name__ == "__main__":
+    l1, l2 = obtain_id_lists(r"input.txt", "   ")
+    result = obtain_total_distance(l1, l2)
+    print(result)
